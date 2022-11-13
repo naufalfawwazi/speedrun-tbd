e0e6780723d28e9a6d16e498ffeeb2f2ee20f5f3
Jeremyas Cornelis <jeremyas.cornelis@gmail.com>
2022-11-13 18:54:29 +0700

    insertMany

---
+++ functions/insertMany.py
@@ -0,0 +1,8 @@
+def insertMany(connection, sql, val):
+    cursor = connection.cursor()
+    try:
+        cursor.executemany(sql, val)
+        connection.commit()
+        print("Data berhasil masuk!")
+    except Error as err:
+        print(f"Error: '{err}'")
