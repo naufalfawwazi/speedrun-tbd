8d1331a9b50b128dc56542cfe0978f587cab1097
Jeremyas Cornelis <jeremyas.cornelis@gmail.com>
2022-11-13 19:56:00 +0700

    display + testDisplay Func done

---
+++ functions/display.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+
+def read_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as err:
+        print(f"Error:  '{err}'")
---
+++ testing/test-display.py
@@ -0,0 +1,30 @@
+from importlib import util as iutil
+
+lib1 = iutil.spec_from_file_location(
+    "connectDB", "C:/xampp/htdocs/collaborations/speedrun-tbd/functions/connectDB.py")
+lib2 = iutil.spec_from_file_location(
+    "insertMany", "C:/xampp/htdocs/collaborations/speedrun-tbd/functions/display.py")
+
+connectDB = iutil.module_from_spec(lib1)
+displayData = iutil.module_from_spec(lib2)
+
+lib1.loader.exec_module(connectDB)
+lib2.loader.exec_module(displayData)
+
+# test connection successful
+connect = connectDB.connection("localhost", "root", "root", "db_tokomainan")
+
+sqlPembeli = '''
+    SELECT *
+    FROM tbl_pembeli;
+    '''
+
+sqlProduk = '''
+    SELECT *
+    FROM tbl_produk;
+    '''
+
+results = displayData.read_query(connect, sqlProduk)
+
+for result in results:
+    print(result)
